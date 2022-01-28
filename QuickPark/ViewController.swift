eeee9a7b41e5ef61ec26d024f51519653c1840e5
G3 <Aljawharayr@gmail.com>
2022-01-28 20:40:37 +0300

    Initial Commit

---
+++ QuickPark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickPark
+//
+//  Created by MAC on 25/06/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
