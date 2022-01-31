8599433bc530d6c27da64bae2cee95f92bfc48c1
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-01-31 20:11:46 +0300

    Admin Add Area1

--- QuickPark/AddAreaViewController.swift
+++ QuickPark/AddAreaViewController.swift
@@ -6,18 +6,40 @@
 //
 
 import UIKit
+import CoreLocation
 import MapKit
 
+
 class AddAreaViewController: UIViewController {
+    private var locationManager = CLLocationManager()
+    
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate=self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
     
     @IBOutlet var mapPin: UIView!
     
+    @IBOutlet var areaNmaeTextField: UIView!
+}
+
+extension AddAreaViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let areaLocation = locations.last
+        print (areaLocation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
