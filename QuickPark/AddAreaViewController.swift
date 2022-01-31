498376c8c943721c8715e57be2cb87800d4546d2
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-01-31 19:47:09 +0300

    UI Add2

---
+++ QuickPark/AddAreaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddAreaViewController.swift
+//  QuickPark
+//
+//  Created by manar . on 31/01/2022.
+//
+
+import UIKit
+import MapKit
+
+class AddAreaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBOutlet var mapPin: UIView!
+    
+}
