ffb49bf02c2c86858ea061b3cf22f79e5a970a63
DeemaAlherbish <Deemaalherbish@gmail.com>
2022-02-05 20:34:25 +0300

    adding animation

--- QuickPark/ViewController.swift
+++ QuickPark/ViewController.swift
@@ -32,8 +32,24 @@ class ViewController: UIViewController{
         super.viewDidLayoutSubviews()
         imageView.center = view.center
         
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            self.animate()
+        })
     }
-    
+    private func animate(){
+        UIView.animate(withDuration: 1, animations: {
+            let size = self.view.frame.size.width * 2.5
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - size
+            self.imageView.frame = CGRect(
+                x: -(diffX/2),
+                y: (diffY/2),
+                width: size,
+                height: size)
+            
+        
+        })
+    }
 }
     extension ViewController: UITableViewDelegate, UITableViewDataSource{
         func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
