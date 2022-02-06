b6e23d2c23ea31a9f845b47ceb26a31f68417193
ghaliakeel <ghaliakeel@gmail.com>
2022-02-06 10:16:19 +0300

    committing to change branch

---
+++ QuickPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickPark
+//
+//  Created by MAC on 25/06/1443 AH.
+//
+
+import UIKit
+import FirebaseDatabase
+class ViewController: UIViewController {
+    private let database = Database.database().reference()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
