256b0426799d6736ecc3d7b631eead48b078fa8b
DeemaaMA <deemaa.ma21@gmail.com>
2022-02-11 01:53:54 +0300

    Connect to database

--- QuickPark/ViewController.swift
+++ QuickPark/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 import FirebaseDatabase
 class ViewController: UIViewController{
-    let parkings = ["King Saud University" , "Imam University" , "Dallah Hospital"]
+  //  let parkings = ["King Saud University" , "Imam University" , "Dallah Hospital"]
    
+    var parkings = [Area]()
+       
+
+    @IBOutlet weak var ParkingView: UIView!
     @IBOutlet weak var ParkingsViews: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         ParkingsViews?.delegate = self
@@ -19,11 +24,24 @@ class ViewController: UIViewController{
         ParkingsViews?.separatorStyle = .none
         ParkingsViews?.showsVerticalScrollIndicator = false
         
-        
-      
-        
+        getParkings()
+
     }
-  
+    private func getParkings() {
+        let ref = Database.database().reference()
+        ref.child("Areas").observe(DataEventType.value, with: { [self] snapshots in
+            print(snapshots.childrenCount)
+        
+            for snapshot in snapshots.children.allObjects as! [DataSnapshot] {
+                let dictionary = snapshot.value as? NSDictionary
+                let area = Area(areaname: dictionary?["areaname"] as? String ?? "", loactionLat: "", locationLong: "", spotNo: "", logo: "")
+                parkings.append(area)
+            }
+            
+            ParkingsViews.reloadData()
+        })
+    }
+
 }
     extension ViewController: UITableViewDelegate, UITableViewDataSource{
         func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -52,17 +70,17 @@ class ViewController: UIViewController{
         
         
         
+        
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = ParkingsViews.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
             let parking = parkings[indexPath.row]
-            cell.Logos.image = UIImage(named: parking)
-            cell.Label.text = parking
-            cell.ParkingView.layer.cornerRadius = 20 //cell.ParkingView.frame.height / 2
-            cell.Logos.layer.cornerRadius = 20 //cell.Logos.frame.height / 2
+            cell.Logos.image = UIImage(named: "King Saud University")
+            cell.Label.text = parking.areaname
+            //cell.ParkingView.layer.cornerRadius = 20 //cell.ParkingView.frame.height / 2
+            //cell.Logos.layer.cornerRadius = 20 //cell.Logos.frame.height / 2
            // let borderColor: UIColor =  (parkings[indexPath.row] == " ") ? .red : UIColor(red: 0/225, green: 144/255, blue: 205/255, alpha: 1) //
-            (parkings[indexPath.row] == " ") ? (cell.Alert.text = "No Available Parkings") : (cell.Alert.text = " ")
+            (parking.areaname == " ") ? (cell.Alert.text = "No Available Parkings") : (cell.Alert.text = " ")
             
-               
 
             return cell
         }
