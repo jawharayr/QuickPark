4d660f45bf4d0c57178f1cd7d6716a7fb7bae008
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-02-10 10:18:49 +0300

    UI2

---
+++ QuickPark/Controller/AccountViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AccountViewController.swift
+//  QuickPark
+//
+//  Created by manar . on 10/02/2022.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    @IBAction func PasswordTextField(_ sender: Any) {
+    }
+    @IBAction func RPasswordTextField(_ sender: Any) {
+    }
+    @IBAction func EmailTextField(_ sender: CustomTextField) {
+    }
+    @IBAction func NameTextField(_ sender: CustomTextField) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
