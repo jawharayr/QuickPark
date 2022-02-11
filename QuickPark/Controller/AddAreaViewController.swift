d6c7b5a649ea82268cc99609280f01f7920aac9f
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-02-12 01:49:07 +0300

    Completing the adding form

--- QuickPark/Controller/AddAreaViewController.swift
+++ QuickPark/Controller/AddAreaViewController.swift
@@ -10,18 +10,21 @@ import CoreLocation
 import MapKit
 import Firebase
 import FirebaseStorage
+import MobileCoreServices
 
 
-class AddAreaViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AddAreaViewController: UIViewController {
     private var locationManager = CLLocationManager()
     private let database = Database.database().reference()
-    private let storage = Storage.storage().reference()
+    
     @IBOutlet weak var AreaNameTextField: UITextField!
     @IBOutlet weak var SpotNoTextField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
     var areaCoordinate:  CLLocationCoordinate2D? = nil
-    var areaName:String = ""
-    var imagePickerController = UIImagePickerController()
+    
+    var areaName: String = "King Khalid Airport"
+    
+   
    
     
   
@@ -29,7 +32,7 @@ class AddAreaViewController: UIViewController, UIImagePickerControllerDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        imagePickerController.delegate = self
+         
         let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
         mapView.addGestureRecognizer(longTapGesture)
         
@@ -39,12 +42,13 @@ class AddAreaViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     @IBAction func SaveAreaButton(_ sender: UIButton) {
         print("SaveAreaButton is pressed")
+        
         let areaName = AreaNameTextField.text
         let spotNo = SpotNoTextField.text
         let areaLat = areaCoordinate?.latitude
         let areaLong = areaCoordinate?.longitude
-       
         if areaName == "" || spotNo == ""
+            
         { print("empty fields")
             
             
@@ -60,9 +64,14 @@ class AddAreaViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBAction func chooseImageButton() {
         print("Add image button was pressed")
         
-        imagePickerController.sourceType = .photoLibrary
-        imagePickerController.allowsEditing = true
-        present(imagePickerController, animated: true)
+        // Must import `MobileCoreServices`
+            let imageMediaType = kUTTypeImage as String
+            // Define and present the `UIImagePickerController`
+            let pickerController = UIImagePickerController()
+            pickerController.sourceType = .photoLibrary
+            pickerController.mediaTypes = [imageMediaType]
+            pickerController.delegate = self
+            present(pickerController, animated: true, completion: nil)
      
     }
     
@@ -88,6 +97,44 @@ class AddAreaViewController: UIViewController, UIImagePickerControllerDelegate, 
         
     }
     
+    @IBAction func uploadimageTapped (_sender: Any) {
+        
+    }
+    
+    func checkPermission () {
+        
+    }
+    
+    func uploadFile(fileUrl: URL) {
+      do {
+        // Create file name
+        let metaData = StorageMetadata()
+        let fileExtension = fileUrl.pathExtension
+        let fileName = ("AreasImages/"+areaName+".\(fileExtension)")
+
+        let storageReference = Storage.storage().reference().child(fileName)
+          
+        let currentUploadTask = storageReference.putFile(from: fileUrl, metadata: metaData) { (storageMetaData,error) in
+          if let error = error {
+            print("Upload error: \(error.localizedDescription)")
+            return
+          }
+                                                                                    
+          // Show UIAlertController here
+          print("Image file: \(fileName) is uploaded! View it at Firebase console!")
+                                                                                    
+          storageReference.downloadURL { (url, error) in
+            if let error = error  {
+              print("Error on getting download url: \(error.localizedDescription)")
+              return
+            }
+            print("Download url of \(fileName) is \(url!.absoluteString)")
+          }
+        }
+      } catch {
+        print("Error on extracting data from url: \(error.localizedDescription)")
+      }
+    }
     
     
 }
@@ -124,3 +171,17 @@ func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, callou
 }
 } //end MKMapDelegate extension
 
+//imp the ImagePickerController
+extension AddAreaViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    // Check for the media type
+    let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! CFString
+    if mediaType == kUTTypeImage {
+      let imageURL = info[UIImagePickerController.InfoKey.imageURL] as! URL
+      // Handle your logic here, e.g. uploading file to Cloud Storage for Firebase
+        uploadFile(fileUrl: imageURL)
+    }
+
+    picker.dismiss(animated: true, completion: nil)
+  }
+}
