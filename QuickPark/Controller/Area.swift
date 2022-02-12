681b9c471aa9eab5ec628668e3ccb2a7c9e236b4
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-02-12 18:28:46 +0300

    AreasList

---
+++ QuickPark/Controller/Area.swift
@@ -0,0 +1,17 @@
+//
+//  Area.swift
+//  QuickPark
+//
+//  Created by manar . on 12/02/2022.
+//
+
+import Foundation
+
+
+struct Area {
+    var areaname: String
+    var loactionLat: String
+    var locationLong: String
+    var spotNo: String
+    var logo: String
+}
