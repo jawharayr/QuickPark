aaea48dba75fb3fc2ee07b291499329cfe8d6b6f
ghaliakeel <ghaliakeel@gmail.com>
2022-02-13 05:59:40 +0300

    //connection updates

--- QuickPark/Auth/TermsViewController.swift
+++ QuickPark/Auth/TermsViewController.swift
@@ -15,15 +15,30 @@ class TermsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+     @IBAction func ForgotPass(_ sender: Any) {
+       
+         let forgotPasswordAlert = UIAlertController(title: "Forgot password?", message: "Enter email address", preferredStyle: .alert)
+            forgotPasswordAlert.addTextField { (textField) in
+                textField.placeholder = "Enter email address"
+            }
+            forgotPasswordAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            forgotPasswordAlert.addAction(UIAlertAction(title: "Reset Password", style: .default, handler: { (action) in
+                let resetEmail = forgotPasswordAlert.textFields?.first?.text
+                Auth.auth().sendPasswordReset(withEmail: resetEmail!, completion: { (error) in
+                    if error != nil{
+                        self.showAlert(title: "Error", message: "Please enter a valid email!")
+                    }else {
+                        self.showAlert(title: "Password reset successful", message: "You have successfully requested to reset your password! please check your email ")
+                    }
+                })
+            }))
+            //PRESENT ALERT
+            self.present(forgotPasswordAlert, animated: true, completion: nil)
+        
+     }
     */
 
 }
