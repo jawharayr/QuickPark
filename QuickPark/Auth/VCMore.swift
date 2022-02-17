0b7ce732a5a85a18690b39f8429e6bc00cc449f1
ghaliakeel <ghaliakeel@gmail.com>
2022-02-17 15:25:18 +0300

    added vc more cnontroller

---
+++ QuickPark/Auth/VCMore.swift
@@ -0,0 +1,25 @@
+//
+//  VCMore.swift
+//  QuickPark
+//
+//  Created by Ghaliah Binakeel on 17/02/2022.
+//
+
+import UIKit
+import SVProgressHUD
+
+class VCMore : ViewController {
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func logoutButtonTouched(_ sender: Any) {
+        SVProgressHUD.show(withStatus: "Logging Out...")
+        FBAuth.logout { success, error in
+            if success == false, let e = error {
+                SVProgressHUD.showError(withStatus: e.localizedDescription)
+            } else {
+                SVProgressHUD.showSuccess(withStatus: "Logged Out.")
+                SceneDelegate.sceneDelegate.setUpHome()
+            }
+        }
+    }
+}
