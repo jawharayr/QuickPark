e52db15bde2de5bf296096ac452365ec3f44ae7d
DeemaAlherbish <Deemaalherbish@gmail.com>
2022-02-19 23:32:53 +0300

    making the logo

--- QuickPark/ViewController.swift
+++ QuickPark/ViewController.swift
@@ -9,60 +9,20 @@ import UIKit
 import FirebaseDatabase
 class ViewController: UIViewController{
    
-    //DEEMA
-    private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
-        imageView.image = UIImage(named: "logo")
-        return imageView
-        
-    }()
-    //DEEMA END HERE
+    //1
     
-    @IBOutlet weak var ParkingsViews: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        view.addSubview(imageView)
+        //2
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        imageView.center = view.center
-        
-        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
-            self.animate()
-        })
+//3
+    //4
     }
-    private func animate(){
-        UIView.animate(withDuration: 1, animations: {
-            let size = self.view.frame.size.width * 3.5
-            let diffX = size - self.view.frame.size.width
-            let diffY = self.view.frame.size.height - size
-            self.imageView.frame = CGRect(
-                x: -(diffX/2),
-                y: (diffY/2),
-                width: size,
-                height: size)
-                    
-        })
-        
-        UIView.animate(withDuration: 1 , animations: {
-           
-            self.imageView.alpha = 0
-        }, completion: { done in
-            if done {
-                DispatchQueue.main.asyncAfter(deadline: .now()+0.3, execute: {
-                    //why are you putting the main pages code in view contorller!!
-                let viewController = ViewController()
-                viewController.modalTransitionStyle = .crossDissolve
-                    viewController.modalPresentationStyle = .fullScreen
-                self.present(viewController, animated: true)
-            })
-                
-            }
-        })
-        
-    }
+    //5
 }
 
 
