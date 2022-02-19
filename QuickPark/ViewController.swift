666be310ebd4021ef668b1caeecee5a1130dd922
DeemaAlherbish <Deemaalherbish@gmail.com>
2022-02-19 23:27:12 +0300

    fixing previous errors

--- QuickPark/ViewController.swift
+++ QuickPark/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import FirebaseDatabase
 class ViewController: UIViewController{
-    let parkings = ["King Saud University" , "Imam University" , "Dallah Hospital"]
    
     //DEEMA
     private let imageView: UIImageView = {
@@ -22,10 +21,7 @@ class ViewController: UIViewController{
     @IBOutlet weak var ParkingsViews: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // causes error!!
-        ParkingsViews.delegate = self
-        ParkingsViews.dataSource = self
-        
+      
         view.addSubview(imageView)
     }
     
@@ -68,23 +64,6 @@ class ViewController: UIViewController{
         
     }
 }
-    extension ViewController: UITableViewDelegate, UITableViewDataSource{
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 120
-        }
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return parkings.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = ParkingsViews.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
-        
-            let parking = parkings[indexPath.row]
-            cell.Logos.image = UIImage(named: parking)
-            cell.Label.text = parking
-            return cell
-        }
-    }
 
 
 
