d07fd42dba1682c07dc9395997f72a158c8a5ec0
DeemaAlherbish <Deemaalherbish@gmail.com>
2022-02-19 23:54:08 +0300

    animation func creation

--- QuickPark/ViewController.swift
+++ QuickPark/ViewController.swift
@@ -25,9 +25,26 @@ class ViewController: UIViewController{
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.center = view.center
-     
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+                  self.animate()
+                })
     }
    
+    private func animate(){
+        UIView.animate(withDuration: 1, animations: {
+                    let size = self.view.frame.size.width * 3.5
+                    let diffX = size - self.view.frame.size.width
+                    let diffY = self.view.frame.size.height - size
+                    self.imageView.frame = CGRect(
+                        x: -(diffX/2),
+                        y: (diffY/2),
+                        width: size,
+                        height: size)
+                            
+                })
+    
+        
+    }
 }
 
 
