03ac0f3c46927cdfeac9b929cc3c129bc93cc31b
DeemaAlherbish <Deemaalherbish@gmail.com>
2022-03-19 19:15:32 +0300

    paypal

--- QuickPark/PayAbleViewController.swift
+++ QuickPark/PayAbleViewController.swift
@@ -29,28 +29,28 @@ class PayAbleViewController: UIViewController {
         lblExtra.text = extra + " SAR"
         lblTotal.text = total + " SAR"
         mainView.layer.cornerRadius = 20
-
+        
         //Paypal
         braintreeClient = BTAPIClient(authorization: "sandbox_5rv25jbw_qf575jr29ngyc4r9")
     }
     
     @IBAction func payTapped(){
-        /* if let image = generateQRCode(using: "test"){
-         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
-         vc.image = image
-         vc.reservation = self.reservation
-         vc.modalPresentationStyle = .overFullScreen
-         self.present(vc, animated: true, completion: nil) */
+        if let image = generateQRCode(using: "test"){
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
+            vc.image = image
+            vc.reservation = self.reservation
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true, completion: nil)
+        }
         
         let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
-        // payPalDriver.BTViewControllerPresentingDelegatev = self //causes an error!
+        // payPalDriver.viewControllerPresentingDelegatev = self //causes an error!
         let request = BTPayPalCheckoutRequest(amount: total)
         request.currencyCode = "USD" // Optional; see BTPayPalCheckoutRequest.h for more options
-        
+
         payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
             if let tokenizedPayPalAccount = tokenizedPayPalAccount {
                 print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
-                
                 // Access additional information
                 let email = tokenizedPayPalAccount.email
                 let firstName = tokenizedPayPalAccount.firstName
@@ -60,7 +60,6 @@ class PayAbleViewController: UIViewController {
                 // See BTPostalAddress.h for details
                 let billingAddress = tokenizedPayPalAccount.billingAddress
                 let shippingAddress = tokenizedPayPalAccount.shippingAddress
-                
             } else if let error = error {
                 // Handle error here...
                 print(error)
@@ -68,21 +67,22 @@ class PayAbleViewController: UIViewController {
                 // Buyer canceled payment approval
             }
         }
+    }
+    
+    func generateQRCode(using string:String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
         
-        func generateQRCode(using string:String) -> UIImage? {
-            let data = string.data(using: String.Encoding.ascii)
-            
-            if let filter = CIFilter(name: "CIQRCodeGenerator"){
-                filter.setValue( data, forKey: "inputMessage")
-                let transform = CGAffineTransform(scaleX: 3, y: 3)
-                if let output = filter.outputImage?.transformed(by: transform){
-                    return UIImage(ciImage: output)
-                }
+        if let filter = CIFilter(name: "CIQRCodeGenerator"){
+            filter.setValue( data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            if let output = filter.outputImage?.transformed(by: transform){
+                return UIImage(ciImage: output)
             }
-            return nil
         }
+        return nil
     }
 }
+
 
 // PayPal
 
