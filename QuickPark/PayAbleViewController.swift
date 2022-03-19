0574d06c219866a02f2fa5f835c59767064bbbbe
DeemaAlherbish <Deemaalherbish@gmail.com>
2022-03-19 17:35:26 +0300

    pay pal final

--- QuickPark/PayAbleViewController.swift
+++ QuickPark/PayAbleViewController.swift
@@ -9,13 +9,10 @@ import UIKit
 import Braintree
 
 class PayAbleViewController: UIViewController {
-
-    
     @IBOutlet weak var lblPrice:UILabel!
     @IBOutlet weak var lblExtra:UILabel!
     @IBOutlet weak var lblTotal:UILabel!
     @IBOutlet weak var mainView:UIView!
-    
     
     var price = ""
     var total = ""
@@ -27,72 +24,64 @@ class PayAbleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         lblPrice.text = price + " SAR"
         lblExtra.text = extra + " SAR"
         lblTotal.text = total + " SAR"
         mainView.layer.cornerRadius = 20
-        
-        
+
         //Paypal
-       braintreeClient = BTAPIClient(authorization: "sandbox_5rv25jbw_qf575jr29ngyc4r9")
-        
+        braintreeClient = BTAPIClient(authorization: "sandbox_5rv25jbw_qf575jr29ngyc4r9")
     }
     
-
     @IBAction func payTapped(){
-       /* if let image = generateQRCode(using: "test"){
-            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
-            vc.image = image
-            vc.reservation = self.reservation
-            vc.modalPresentationStyle = .overFullScreen
-            self.present(vc, animated: true, completion: nil) */
+        /* if let image = generateQRCode(using: "test"){
+         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
+         vc.image = image
+         vc.reservation = self.reservation
+         vc.modalPresentationStyle = .overFullScreen
+         self.present(vc, animated: true, completion: nil) */
         
-        
-            let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
+        let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
         // payPalDriver.BTViewControllerPresentingDelegatev = self //causes an error!
-                let request = BTPayPalCheckoutRequest(amount: total)
-                        request.currencyCode = "USD" // Optional; see BTPayPalCheckoutRequest.h for more options
-
-                        payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
-                            if let tokenizedPayPalAccount = tokenizedPayPalAccount {
-                                print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
-
-                                // Access additional information
-                                let email = tokenizedPayPalAccount.email
-                                let firstName = tokenizedPayPalAccount.firstName
-                                let lastName = tokenizedPayPalAccount.lastName
-                                let phone = tokenizedPayPalAccount.phone
-
-                                // See BTPostalAddress.h for details
-                                let billingAddress = tokenizedPayPalAccount.billingAddress
-                                let shippingAddress = tokenizedPayPalAccount.shippingAddress
-                                
-                            } else if let error = error {
-                                // Handle error here...
-                                print(error)
-                            } else {
-                                // Buyer canceled payment approval
-                            }
-    }
-    
-    
-    func generateQRCode(using string:String) -> UIImage? {
+        let request = BTPayPalCheckoutRequest(amount: total)
+        request.currencyCode = "USD" // Optional; see BTPayPalCheckoutRequest.h for more options
         
-        let data = string.data(using: String.Encoding.ascii)
-        
-        if let filter = CIFilter(name: "CIQRCodeGenerator"){
-            filter.setValue( data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 3, y: 3)
-            if let output = filter.outputImage?.transformed(by: transform){
-                return UIImage(ciImage: output)
+        payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
+            if let tokenizedPayPalAccount = tokenizedPayPalAccount {
+                print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
+                
+                // Access additional information
+                let email = tokenizedPayPalAccount.email
+                let firstName = tokenizedPayPalAccount.firstName
+                let lastName = tokenizedPayPalAccount.lastName
+                let phone = tokenizedPayPalAccount.phone
+                
+                // See BTPostalAddress.h for details
+                let billingAddress = tokenizedPayPalAccount.billingAddress
+                let shippingAddress = tokenizedPayPalAccount.shippingAddress
+                
+            } else if let error = error {
+                // Handle error here...
+                print(error)
+            } else {
+                // Buyer canceled payment approval
             }
         }
-        return nil
         
+        func generateQRCode(using string:String) -> UIImage? {
+            let data = string.data(using: String.Encoding.ascii)
+            
+            if let filter = CIFilter(name: "CIQRCodeGenerator"){
+                filter.setValue( data, forKey: "inputMessage")
+                let transform = CGAffineTransform(scaleX: 3, y: 3)
+                if let output = filter.outputImage?.transformed(by: transform){
+                    return UIImage(ciImage: output)
+                }
+            }
+            return nil
+        }
     }
-    
-}
 }
 
 // PayPal
