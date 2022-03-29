d177da067da70582b1562af1843ad22e1b83b581
Manar <ManarAldhafyan@gmail.com>
2022-03-29 22:50:43 +0300

    added code to load and save char messages

--- QuickPark/Chat/ChatViewController.swift
+++ QuickPark/Chat/ChatViewController.swift
@@ -35,6 +35,123 @@ class VCChat: MessagesViewController, InputBarAccessoryViewDelegate, MessagesDat
         loadChat()
         
     }
+    
+    // MARK: - Custom messages handlers
+    
+    func createNewChat() {
+        let users = [self.currentUser.uid, self.user2UID]
+         let data: [String: Any] = [
+             "users":users
+         ]
+         
+         let db = Firestore.firestore().collection("Chats")
+         db.addDocument(data: data) { (error) in
+             if let error = error {
+                 print("Unable to create chat! \(error)")
+                 return
+             } else {
+                 self.loadChat()
+             }
+         }
+    }
+    
+    func loadChat() {
+        
+        //Fetch all the chats which has current user in it
+        let db = Firestore.firestore().collection("Chats")
+                .whereField("users", arrayContains: Auth.auth().currentUser?.uid ?? "Not Found User 1")
+        
+        
+        db.getDocuments { (chatQuerySnap, error) in
+            
+            if let error = error {
+                print("Error: \(error)")
+                return
+            } else {
+                
+                //Count the no. of documents returned
+                guard let queryCount = chatQuerySnap?.documents.count else {
+                    return
+                }
+                
+                if queryCount == 0 {
+                    //If documents count is zero that means there is no chat available and we need to create a new instance
+                    self.createNewChat()
+                }
+                else if queryCount >= 1 {
+                    //Chat(s) found for currentUser
+                    for doc in chatQuerySnap!.documents {
+                        
+                        let chat = Chat(dictionary: doc.data())
+                        //Get the chat which has user2 id
+                        if (chat?.users.contains(self.user2UID!))! {
+                            
+                            self.docReference = doc.reference
+                            //fetch it's thread collection
+                             doc.reference.collection("thread")
+                                .order(by: "created", descending: false)
+                                .addSnapshotListener(includeMetadataChanges: true, listener: { (threadQuery, error) in
+                            if let error = error {
+                                print("Error: \(error)")
+                                return
+                            } else {
+                                self.messages.removeAll()
+                                    for message in threadQuery!.documents {
+
+                                        let msg = Message(dictionary: message.data())
+                                        self.messages.append(msg!)
+                                        print("Data: \(msg?.content ?? "No message found")")
+                                    }
+                                self.messagesCollectionView.reloadData()
+                                self.messagesCollectionView.scrollToLastItem(at: .bottom, animated: true)
+                            }
+                            })
+                            return
+                        } //end of if
+                    } //end of for
+                    self.createNewChat()
+                } else {
+                    print("Let's hope this error never prints!")
+                }
+            }
+        }
+    }
+    
+    
+    private func insertNewMessage(_ message: Message) {
+        
+        messages.append(message)
+        messagesCollectionView.reloadData()
+        
+        DispatchQueue.main.async {
+            self.messagesCollectionView.scrollToLastItem(at: .bottom, animated: true)
+        }
+    }
+    
+    private func save(_ message: Message) {
+        
+        let data: [String: Any] = [
+            "content": message.content,
+            "created": message.created,
+            "id": message.id,
+            "senderID": message.senderID,
+            "senderName": message.senderName
+        ]
+        
+        docReference?.collection("thread").addDocument(data: data, completion: { (error) in
+            
+            if let error = error {
+                print("Error Sending message: \(error)")
+                return
+            }
+            
+            self.messagesCollectionView.scrollToLastItem(at: .bottom, animated: true)
+            
+        })
+    }
+    
+    // MARK: - InputBarAccessoryViewDelegate
+    
             func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
 
                 let message = Message(id: UUID().uuidString, content: text, created: Timestamp(), senderID: currentUser.uid, senderName: currentUser.displayName!)
@@ -48,6 +165,32 @@ class VCChat: MessagesViewController, InputBarAccessoryViewDelegate, MessagesDat
                   messagesCollectionView.scrollToBottom(animated: true)
             }
     
+    
+    // MARK: - MessagesDataSource
+    func currentSender() -> SenderType {
+        
+        return Sender(id: Auth.auth().currentUser!.uid, displayName: Auth.auth().currentUser?.displayName ?? "Name not found")
+        
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        
+        return messages[indexPath.section]
+        
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        
+        if messages.count == 0 {
+            print("No messages to display")
+            return 0
+        } else {
+            return messages.count
+        }
+    }
+    
+    
+    // MARK: - MessagesLayoutDelegate
     
     func avatarSize(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGSize {
         return .zero
