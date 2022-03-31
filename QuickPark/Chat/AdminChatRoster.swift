d13cc773b99f3702eb99041d2045fae6b9d8c3a1
ManarAldhafyan <97637723+ManarAldhafyan@users.noreply.github.com>
2022-03-31 22:07:20 +0300

    Added admin chat roster view

---
+++ QuickPark/Chat/AdminChatRoster.swift
@@ -0,0 +1,14 @@
+//
+//  AdminChatRoster.swift
+//  QuickPark
+//
+//  Created by manar . on 31/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class AdminChatRoster : UITableViewController {
+    //si_rosterToChat
+    //sbi_adminChatRoster
+}
