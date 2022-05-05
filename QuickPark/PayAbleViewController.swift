e18aca95b2b16f36cff02e259a8ded364da3ba41
Ghaliah <ghaliakeel@gmail.com>
2022-05-05 11:01:42 +0530

    Update PayAbleViewController.swift
    - added pay button outlet
    - added function to inform expired qrcode
    - added compute function

--- QuickPark/PayAbleViewController.swift
+++ QuickPark/PayAbleViewController.swift
@@ -10,13 +10,13 @@ import Firebase
 import Braintree
 import FirebaseDatabase
 
-
 class PayAbleViewController: UIViewController {
     @IBOutlet weak var lblPrice:UILabel!
     @IBOutlet weak var lblExtra:UILabel!
     @IBOutlet weak var lblTotal:UILabel!
     @IBOutlet weak var mainView:UIView!
     
+    @IBOutlet weak var payButton: UIButton!
     @IBOutlet weak var KSULabel: UILabel!
     var price = ""
     var total = ""
@@ -24,72 +24,41 @@ class PayAbleViewController: UIViewController {
     var reservation:Reservation!
     
     private let database = Database.database().reference()
-    
-    //for paypal
     var braintreeClient: BTAPIClient!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        lblPrice.text = price + " SAR"
-        lblExtra.text = extra + " SAR"
-        lblTotal.text = total + " SAR"
         if reservation.area == ""{
             KSULabel.text = "King Saud University"
         }else {
             KSULabel.text = reservation.area
         }
         mainView.layer.cornerRadius = 20
-        
-        //Paypal
         braintreeClient = BTAPIClient(authorization: "sandbox_5rv25jbw_qf575jr29ngyc4r9")
+        
+        displayValue()
     }
     
+    func displayValue() {
+        lblPrice.text = price + " SAR"
+        lblExtra.text = extra + " SAR"
+        lblTotal.text = total + " SAR"
+    }
     
     func track(qrcode code: String){
         Database.database().reference().child("QRCode").child(code).observe(.value) { dataSnap in
             if dataSnap.exists(){
                 guard let reserDict = dataSnap.value as? [String:Any] else{return}
-                //  print("Iterating on QRCode dictionary: ",reserDict)
                 if let isScanned = reserDict["isScanned"] as? Bool, isScanned{
                     self.dismiss(animated: false, completion: nil)
                     QPLNSupport.remove(self.reservation.id)
                 }
             }
         }
-        //        Database.database().reference().child("QRCode").child(code).observeSingleEvent(of: .value, with: )
     }
-//    
-//    @IBAction func payTapped(){
-//        
-//        let unique = String("\(Date().timeIntervalSince1970)").replacingOccurrences(of: ".", with: "")
-//        print("My unique QR code: ",unique)
-//        if let image = UIImage.generateQRCode(using: unique){
-//            
-//            let object: [String : Any] = ["isScanned":false]
-//            
-//            database.child("QRCode").child(unique).setValue(object) { error, ref in
-//                print("Error wihle saving QRCode to Firebase. Error= ",error?.localizedDescription)
-//            }
-//            
-//            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
-//            vc.image = image
-//            vc.reservation = self.reservation
-//            vc.exitQRCode = unique
-//            vc.modalPresentationStyle = .overFullScreen
-//            self.present(vc, animated: true, completion: nil)
-//        }
-//    }
     
     @IBAction func payTapped(){
-        /*    if let image = generateQRCode(using: "test"){
-         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
-         vc.image = image
-         vc.reservation = self.reservation
-         vc.modalPresentationStyle = .overFullScreen
-         self.present(vc, animated: true, completion: nil)
-         } */
-
         let payPalDriver = BTPayPalDriver(apiClient: braintreeClient)
         let request = BTPayPalCheckoutRequest(amount: total)
         //let request = BTPayPalCheckoutRequest(amount: "90")//test line
@@ -98,49 +67,72 @@ class PayAbleViewController: UIViewController {
         payPalDriver.tokenizePayPalAccount(with: request) { (tokenizedPayPalAccount, error) in
             if let tokenizedPayPalAccount = tokenizedPayPalAccount {
                 print("Got a nonce: \(tokenizedPayPalAccount.nonce)")
-                // Access additional information
-                let email = tokenizedPayPalAccount.email
+                /*let email = tokenizedPayPalAccount.email
                 let firstName = tokenizedPayPalAccount.firstName
                 let lastName = tokenizedPayPalAccount.lastName
                 let phone = tokenizedPayPalAccount.phone
-
+                
                 // See BTPostalAddress.h for details
                 let billingAddress = tokenizedPayPalAccount.billingAddress
-                let shippingAddress = tokenizedPayPalAccount.shippingAddress
+                let shippingAddress = tokenizedPayPalAccount.shippingAddress*/
                 
                 self.openExitQRCodePage()
             } else if let error = error {
-                // Handle error here...
                 print(error)
             } else {
-                // Buyer canceled payment approval
+                print("Buyer canceled payment approval")
             }
         }
     }
     
-    
     func openExitQRCodePage(){
-        let unique = String("\(Date().timeIntervalSince1970)").replacingOccurrences(of: ".", with: "")
-        print("My unique QR code: ",unique)
-        if let image = generateQRCode(using: unique){
-            
+        let uniqueQRCode = String("\(Date().timeIntervalSince1970)").replacingOccurrences(of: ".", with: "")
+        if let image = generateQRCode(using: uniqueQRCode){
             let object: [String : Any] = ["isScanned":false]
-            database.child("QRCode").child(unique).setValue(object) { error, ref in
-                print("Error wihle saving QRCode to Firebase. Error= ",error?.localizedDescription)
+            database.child("QRCode").child(uniqueQRCode).setValue(object) { error, ref in
+                print("Error wihle saving QRCode to Firebase. Error= ",error?.localizedDescription ?? "Unknown Error")
             }
             
             let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QRCodeVC") as! QRCodeVC
             vc.image = image
             vc.reservation = self.reservation
-            vc.exitQRCode = unique
+            vc.exitQRCode = uniqueQRCode
             vc.modalPresentationStyle = .overFullScreen
             self.present(vc, animated: true, completion: nil)
-        }
-    }
             
+            self.perform(#selector(exitParkingQRCodeExpired), with: self.reservation, afterDelay: TimeInterval(K_QR_Code_Expire_Time))
+            UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: "K_Parking_End_Time")
+        }   
+    }
+    
+    @objc func exitParkingQRCodeExpired() {
+        payButton.setTitle("Pay Margin", for: .normal)
+        
+    }
+    
+    func calculateMargin() {
+        guard let pit = UserDefaults.standard.value(forKey: "K_Parking_End_Time") as? TimeInterval else {
+            price = "0"
+            total = "0"
+            extra = "0"
+            displayValue()
+            return
+        }
+        
+        let p = ((pit / 60) * 15)
+        price = "\(p)"
+        total = "\(p)"
+        extra = "\(0)"
+    }
+    
+    func updateValues() {
+        
+    }
+    
+    
+    
     func generateQRCode(using string:String) -> UIImage? {
         let data = string.data(using: String.Encoding.ascii)
-        
         if let filter = CIFilter(name: "CIQRCodeGenerator"){
             filter.setValue( data, forKey: "inputMessage")
             let transform = CGAffineTransform(scaleX: 3, y: 3)
@@ -151,6 +143,7 @@ class PayAbleViewController: UIViewController {
         return nil
     }
 }
+
 extension CIImage {
     var transparent: CIImage? {
         return inverted?.blackTransparent
