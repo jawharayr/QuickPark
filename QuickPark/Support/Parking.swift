23a1ef385241d7adc541f57bde1b21b4829149e1
Ghaliah <ghaliakeel@gmail.com>
2022-05-08 12:27:28 +0530

    added parking after payment class to load and update data

--- QuickPark/Support/Parking.swift
+++ QuickPark/Support/Parking.swift
@@ -1,3 +1,18 @@
+//
+//  Parking.swift
+//  QuickPark
+//
+//  Created by Ghaliah on 08/05/22.
+//
+
+import Foundation
+
 struct UDKeys {
     static let K_Parking_End_Time = "K_Parking_End_Time"
+}
+
+            return UserDefaults.standard.value(forKey: UDKeys.K_Parking_End_Time)
+        }
+    }
+    
 }
