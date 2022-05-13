12ef7f8c1e10b85706a9a3bbd10fef980797ec59
ghaliah <ghaliakeel@gmail.com>
2022-05-13 08:28:47 +0300

    as previous

--- QuickPark/QRCodeVC.swift
+++ QuickPark/QRCodeVC.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 import FirebaseFirestore
 import SVProgressHUD
 
-let K_QR_Code_Expire_Time =  20 // 900, 30 is for testing only
+let K_QR_Code_Expire_Time =  35 // 900 (15m), 35s is for testing only
 
 class QRCodeVC: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
